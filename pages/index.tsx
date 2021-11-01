30860dde544c61ef94edb8181d4197d63ebe1a86
Imagine96 <81493770+Imagine96@users.noreply.github.com>
2021-11-02 00:14:51 +0100

    Basics/ts tw jest config (#1)
    * initial configurations
    
    * git ignore updated
    
    Co-authored-by: Ruben Guedes <imagine@macbook-air-de-ruben.home>

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <main className='w-full h-screen' >
+      <h1> Magic Mash ui-library is on the way mf!! </h1>
+    </main>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      color:{
+
+      }
+    },
+    fontFamily: {
+      
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
