e6d5d210fc6563cc5ad38a8937943e08a420080f
Ruben Guedes <imagine@macbook-air-de-ruben.home>
2021-11-02 00:59:17 +0100

    tailwind base theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,21 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      color:{
-
+      colors:{
+        'lighBlue': '#D2E4FD',
+        'defaultBlue': '#27ABEB',
+        'darkBlue': '#102041',
+        'lightGreen': '#CDE9D6',
+        'darkGreen': '#025E73',
+        'brown': '#F2C979',
+        'lightYellow': '#FFF1CB',
+        'lightPink': '#F8D3CF',
+        'danger': '#DA4E4F'
       }
     },
     fontFamily: {
-      
+      text: ['Roboto', 'sans-serif'],
+      header: ['Lato', 'sans-serif']
     }
   },
   variants: {
