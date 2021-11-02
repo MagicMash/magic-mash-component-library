b0bec67ebfb2af552b79e3b7f45d0fe5753ee79e
Imagine96 <81493770+Imagine96@users.noreply.github.com>
2021-11-02 01:02:24 +0100

    tailwind base theme (#2)
    Co-authored-by: Ruben Guedes <imagine@macbook-air-de-ruben.home>

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <main className='w-full h-screen' >
-      <h1> Magic Mash ui-library is on the way mf!! </h1>
+    <main className='w-full h-screen flex flex-col justify-center items-center' >
+      <h1 className=' text-darkBlue ' > Magic Mash ui-library is on the way mf!! </h1>
+      <p className='text-darkGreen' > omae mo ... </p>
+      <p className='text-danger ' > MUDAMUDAMUDA ... ! </p>
     </main>
   )
 }
