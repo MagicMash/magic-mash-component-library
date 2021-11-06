01a794b422b3ca2be3813d7607c9715d90535bf1
Imagine96 <81493770+Imagine96@users.noreply.github.com>
2021-11-06 16:39:07 +0100

    Component/alert (#8)
    * useAlert hook
    
    * alert basiscs
    
    Co-authored-by: Ruben Guedes <imagine@macbook-air-de-ruben.home>

---
+++ __test__/hooks/useAlerts.test.js
@@ -0,0 +1,40 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { act } from "react-test-renderer";
+import useAlert from "../../src/hooks/useAlert";
+
+const initialAlerts = [
+  {
+    title: "test1",
+    text: "alert test text",
+  },
+  {
+    title: "test2",
+    text: "alert test text",
+  },
+];
+
+const singleAlert = {
+  title: "test3",
+  text: "alert test text",
+};
+
+describe("useAlert hook", () => {
+  it("returs the alerts and methods to add and remove alerts", () => {
+    const { result } = renderHook(() => useAlert(initialAlerts));
+    expect(result.current[0]).toHaveLength(2);
+
+    act(() => result.current[1](singleAlert));
+    let alerts = result.current[0];
+
+    expect(alerts).toHaveLength(3);
+    expect(alerts[0].title).toEqual("test1");
+    expect(alerts[1].title).toEqual("test2");
+    expect(alerts[2].title).toEqual("test3");
+
+    act(() => result.current[2]("test1"));
+    alerts = result.current[0];
+
+    expect(alerts[0].title).toEqual("test2");
+    expect(alerts[1].title).toEqual("test3");
+  });
+});
---
+++ src/hooks/useAlert.ts
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { alertData } from "../ts/componentsProps";
+
+const useAlert = (initialAlerts: alertData[]) : [alertData[], (alert: alertData) => void, (alertTitleToRemove: string) => void] => {
+
+    const [alerts, setAlerts] = useState(initialAlerts)
+
+    const addAlert = (alert: alertData) => {
+        setAlerts(prevState => {
+            const newState = prevState
+            newState.push(alert)
+            return newState
+        })
+    }
+
+    const removeAlert = (alertTitleToRemove: string) => {
+        setAlerts(prevState => {
+            let newState = prevState
+            newState = newState.filter(alert => {
+                return alert.title !== alertTitleToRemove
+            })
+            return newState
+        })
+    }
+
+    return [alerts, addAlert, removeAlert]
+}
+
+export default useAlert
